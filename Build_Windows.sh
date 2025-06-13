2dd32546a375480b71658684698eff160466e157
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2025-06-13 06:53:09 +0200

    Update Build_Windows.sh

--- Build_Windows.sh
+++ Build_Windows.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # sudo apt-get install mingw-w64 g++-mingw-w64
 
-i686-w64-mingw32-gcc -DSDLGUI main.c -I./windows_deps/include/SDL2 -I./windows_deps/include/libusb-1.0 -L./windows_deps/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lusb-1.0 -mwindows -o MD_Dumper_GUI.exe
-i686-w64-mingw32-gcc main.c -I./windows_deps/include/libusb-1.0 -L./windows_deps/lib -lmingw32 -lusb-1.0 -lcomdlg32 -mconsole -o MD_Dumper.exe
+i686-w64-mingw32-gcc -DSDLGUI main.c -I./windows_deps/include/SDL2 -I./windows_deps/include/libusb-1.0 -L./windows_deps/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lusb-1.0 -mwindows -o SNES_Dumper_GUI.exe
+i686-w64-mingw32-gcc main.c -I./windows_deps/include/libusb-1.0 -L./windows_deps/lib -lmingw32 -lusb-1.0 -lcomdlg32 -mconsole -o SNES_Dumper.exe
 mkdir Windows_Build
 cp -dR Informations.txt ./Windows_Build/
 cp *.csv ./Windows_Build/
-cp MD_Dumper.exe ./Windows_Build/
-cp MD_Dumper_GUI.exe ./Windows_Build/
+cp SNES_Dumper.exe ./Windows_Build/
+cp SNES_Dumper_GUI.exe ./Windows_Build/
 cp -dR images ./Windows_Build/
-cp -dR MD_Dumper_GUI.bat ./Windows_Build/
+cp -dR SNES_Dumper_GUI.bat ./Windows_Build/
 cp ./windows_deps/bin/*.dll ./Windows_Build
