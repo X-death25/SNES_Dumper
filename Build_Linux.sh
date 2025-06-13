573c7e143da170dd9575c980ac0153a100d2b088
AlpasNet <117809035+AlpasNet@users.noreply.github.com>
2025-06-13 06:52:32 +0200

    Update Build_Linux.sh

--- Build_Linux.sh
+++ Build_Linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcc main.c -DSDLGUI -I/usr/include/SDL2 -I/usr/include/libusb-1.0 -L/usr/lib -lSDL2main -lSDL2_image -lSDL2 -lusb-1.0 -o MD_Dumper_GUI
-gcc main.c -I/usr/include/libusb-1.0 -L/usr/lib -lusb-1.0 -o MD_Dumper
+gcc main.c -DSDLGUI -I/usr/include/SDL2 -I/usr/include/libusb-1.0 -L/usr/lib -lSDL2main -lSDL2_image -lSDL2 -lusb-1.0 -o SNES_Dumper_GUI
+gcc main.c -I/usr/include/libusb-1.0 -L/usr/lib -lusb-1.0 -o SNES_Dumper
 mkdir Linux_Build
 cp -dR Informations.txt ./Linux_Build
 cp *.csv ./Linux_Build/
-cp MD_Dumper ./Linux_Build/
-cp MD_Dumper_GUI ./Linux_Build/
+cp SNES_Dumper ./Linux_Build/
+cp SNES_Dumper_GUI ./Linux_Build/
 cp -dR images ./Linux_Build/
