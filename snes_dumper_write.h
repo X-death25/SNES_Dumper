b159f55324d2ff98af0dcad89d0fe062e2b40a00
Bastien <X-death@ultimate-consoles.fr>
2025-07-04 21:29:04 +0200

    Update Erase Algo
    Update Erase Algo

--- snes_dumper_write.h
+++ snes_dumper_write.h
@@ -362,7 +362,70 @@ int Erase_SNES_Flash(int erase_mode)
             chip_id = usb_buffer_in[8];
             flash_id = (manufacturer_id<<8) | chip_id;
 
-            printf(" Flash ID : %04X \n\n",flash_id);
+            printf("Flash ID : %04X \n\n",flash_id);
+
+        for (i = 0; i < chipid_text_values_count; i++)
+		{
+			strncpy(txt_csv_deviceID,chipid_text_values[i],4);
+			//printf(" \n txt chipid value : %s \n",txt_csv_deviceID);
+			csv_deviceID = (unsigned short)strtol(txt_csv_deviceID, NULL, 16);
+			//printf(" \n DEC Device ID value : %ld \n",csv_deviceID);
+			// If found we need to copy all usefull info from CSV to MD dumper Var
+
+			if ( flash_id == csv_deviceID  )
+			{
+				Index_chksm = i;
+				printf("Found chip in CSV Flashlist ! \n");
+				printf("Position in csv table %d \n",i);
+
+				// Flash Size
+				strncpy(txt_csv_flash_size,chipid_text_values[i]+5,3);
+				txt_csv_flash_size[4] = '\0'; // Null-terminate the output string
+				//printf("Txt flash size : %s \n",txt_csv_flash_size);
+				csv_flash_size = (unsigned char)strtol(txt_csv_flash_size, NULL, 10);
+				//printf("CSV Flash Size  %d \n",csv_flash_size);
+				flash_size=1024*csv_flash_size;
+				//printf("La valeur de FlashSize est %ld Ko \n",flash_size);
+
+				// Algo Erase
+				strncpy(txt_csv_erase_algo,chipid_text_values[i]+9,2);
+				txt_csv_erase_algo[2] = '\0'; // Null-terminate the output string
+				//printf("Txt Erase Algo : %s \n",txt_csv_erase_algo);
+				csv_erase_algo = (unsigned char)strtol(txt_csv_erase_algo, NULL, 8);
+				//printf("CSV Erase Algo  %d \n",csv_erase_algo);
+
+				// Write Algo
+				strncpy(txt_csv_write_algo,chipid_text_values[i]+12,2);
+				txt_csv_write_algo[2] = '\0'; // Null-terminate the output string
+				//printf("Txt Write Algo  : %s \n",txt_csv_write_algo);
+				csv_write_algo = (unsigned char)strtol(txt_csv_write_algo, NULL, 8);
+				//printf("CSV Write Algo %d \n",csv_write_algo);
+
+				// Chip Name
+				strncpy(txt_csv_flash_name,chipid_text_values[i]+15,11);
+				txt_csv_flash_name[12] = '\0'; // Null-terminate the output string
+				//printf("Flash Device Reference : %s \n",txt_csv_flash_name);
+
+				// Voltage
+				strncpy(txt_csv_voltage,chipid_text_values[i]+27,2);
+				txt_csv_voltage[2] = '\0'; // Null-terminate the output string
+				//printf("Txt Chip Voltage : %s \n",txt_csv_voltage);
+				csv_voltage = (unsigned char)strtol(txt_csv_voltage, NULL, 8);
+				//printf("CSV Chip Voltage  %d \n",csv_voltage);
+
+				// Manufacturer
+				strncpy(txt_csv_man_name,chipid_text_values[i]+30,18);
+				txt_csv_man_name[19] = '\0'; // Null-terminate the output string
+				//printf("Chip Manufacturer : %s \n",txt_csv_man_name);
+
+				printf("Memory : %s \n",txt_csv_flash_name);
+				printf("Capacity %ld Ko \n",flash_size);
+				printf("Chip Manufacturer : %s \n",txt_csv_man_name);
+				printf("Chip Voltage %ld V \n",csv_voltage);
+				printf("CSV Erase Algo  %d \n",csv_erase_algo);
+				printf("CSV Write Algo %d \n",csv_write_algo);
+			}
+		}
 
 
 
